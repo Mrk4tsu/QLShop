2e394316bacbe74f07c7f28f0ab74fdd03d2f42c
K4tsuJr <120944583+Mrk4tsu@users.noreply.github.com>
2023-06-28 10:05:25 +0700

    Revert "Thêm ReactJs, Thêm Authenticate"
    This reverts commit cf5cace7f588f6c4bc94bb35ef4370b362202e09.

--- Users.BackendAPI/Controllers/ProductsController.cs
+++ Users.BackendAPI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Users.Application.Catalog.Products;
