d8d373e41597a0363558e22f6f9c8bf0156f079e
K4tsuJr <120944583+Mrk4tsu@users.noreply.github.com>
2023-06-29 16:50:01 +0700

    Using Fluent Validation

---
+++ Users.ViewModels/System/Users/LoginRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+namespace Users.ViewModels.System.Users
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Username là phải bắt buộc");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password là phải bắt buộc").MinimumLength(6).WithMessage("Password có độ dài phải lớn hơn 6 kí tự");
+        }
+    }
+}
